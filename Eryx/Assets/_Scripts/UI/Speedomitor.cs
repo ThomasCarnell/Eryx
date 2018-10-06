943f77a9f31c7599bcb3b95060e35ea09ced0b6d
Rasmus Thorup <rasmusbthorup@gmail.com>
2018-10-06 22:47:08 +0200

    Added UI - And Map
    Mute City V1 Added

---
+++ Eryx/Assets/_Scripts/UI/Speedomitor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Speedomitor : MonoBehaviour {
+
+    TextMeshProUGUI speedomitorValue;
+
+	// Use this for initialization
+	void Start () {
+        speedomitorValue = GetComponent<TextMeshProUGUI>();
+    }
+	
+    public void SetSpeedomitor(float speed){
+        speedomitorValue.text = speed.ToString("F0");
+    }
+}
