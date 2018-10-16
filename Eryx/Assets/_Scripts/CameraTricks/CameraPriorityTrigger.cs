4565b4e06ed63a718a416a8e880a8823a6699277
Rasmus Thorup <rasmusbthorup@gmail.com>
2018-10-16 16:51:29 +0200

    Cinemachine pipe setup

---
+++ Eryx/Assets/_Scripts/CameraTricks/CameraPriorityTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraPriorityTrigger : MonoBehaviour {
+
+    public CinemachineVirtualCamera cinemachineCamera;
+
+    public int ontriggerEnterPriority;
+    public int ontriggerExitPriority;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            cinemachineCamera.Priority = ontriggerEnterPriority;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            cinemachineCamera.Priority = ontriggerExitPriority;
+    }
+
+}
