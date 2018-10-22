aedd46ffd2aaa1deaf86e3f2d5d51ef6c6d54079
Rasmus Thorup <rasmusbthorup@gmail.com>
2018-10-23 01:34:14 +0200

    Update graphics
    With ppfx

---
+++ Eryx/Assets/_Scripts/Tools/ScreenResolution.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenResolution : MonoBehaviour {
+
+    public Vector2 screenSize;
+
+    void Start()
+    {
+        Screen.SetResolution((int)screenSize.x, (int)screenSize.y, true);
+    }
+
+}
